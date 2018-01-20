aa3f097ba66e87adabc28ae1bc0e29702924c283
Javivi <Javivi@users.noreply.github.com>
2018-01-20 13:56:42 +0100

    small refactor (getting ready for tests)

--- ws-msgqueue/msgqueue.go
+++ ws-msgqueue/msgqueue.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"net/http"
@@ -10,7 +11,7 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool { //disable after done with js tests
+	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
@@ -18,54 +19,92 @@ var upgrader = websocket.Upgrader{
 var messageQueue = make(chan []byte, 100)
 
 func main() {
-	defer close(messageQueue)
+	ready := make(chan bool, 10)
+	err := initServer("localhost:8080", ready)
+
+	if err != nil {
+		fmt.Printf("ERROR [%s] initialising server\n", err)
+	}
+}
+
+func initServer(addr string, ch chan<- bool) error {
+	defer close(ch)
 
 	http.HandleFunc("/pushmsg", func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+
+		if !ok || username != "hello" || password != "test" {
+			fmt.Printf("ERROR [%s:%s] invalid credentials\n", username, password)
+			return
+		}
+
 		conn, err := upgrader.Upgrade(w, r, nil)
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("ERROR [%s] upgrading connection\n", err)
 			return
 		}
 
 		go func() {
-			defer conn.Close()
-
 			for {
 				_, msg, err := conn.ReadMessage()
 
 				if err != nil {
-					fmt.Println(err)
+					fmt.Printf("ERROR [%s] reading message\n", err)
 					return
 				}
+
 				messageQueue <- msg
+				fmt.Printf("[msgqueue] pushing message: %s\n", msg)
 			}
 		}()
 	})
 
 	http.HandleFunc("/popmsg", func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+
+		if !ok || username != "hello" || password != "test" {
+			fmt.Printf("ERROR [%s:%s] invalid credentials\n", username, password)
+			return
+		}
+
 		conn, err := upgrader.Upgrade(w, r, nil)
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("ERROR [%s] upgrading connection\n", err)
 			return
 		}
 
 		go func() {
-			defer conn.Close()
-
 			for {
 				select {
-				case m := <-messageQueue:
-					conn.WriteMessage(websocket.TextMessage, m)
+				case msg := <-messageQueue:
+					fmt.Printf("[msgqueue] popping message: %s\n", msg)
+					err := conn.WriteMessage(websocket.TextMessage, msg)
+
+					if err != nil {
+						fmt.Printf("ERROR [%s] sending message\n", err)
+						return
+					}
 				}
 			}
 		}()
 	})
 
-	err := http.ListenAndServeTLS("localhost:8080", os.Getenv("WS_CERT_DIR")+"server.crt", os.Getenv("WS_CERT_DIR")+"server.key", nil)
+	certDir := os.Getenv("WS_CERT_DIR")
 
-	if err != nil {
-		fmt.Println(err)
+	if certDir == "" {
+		err := errors.New("initServer: WS_CERT_DIR is not defined")
+		return err
 	}
+
+	ch <- true
+
+	err := http.ListenAndServeTLS(addr, certDir+"server.crt", certDir+"server.key", nil)
+
+	if err != http.ErrServerClosed {
+		return err
+	}
+
+	return nil
 }

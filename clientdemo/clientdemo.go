191cb48c1d23b262904fa7caf369eeaa5910aa87
Javivi <Javivi@users.noreply.github.com>
2018-01-21 20:54:55 +0100

    refactor and make the demo nicer

--- clientdemo/clientdemo.go
+++ clientdemo/clientdemo.go
@@ -3,80 +3,113 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-	"flag"
+	"encoding/base64"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
 
+type Message struct {
+	Topic   string
+	Content string
+}
+
 func main() {
-	demo := flag.String("demo", "", "act as a pub or sub")
-	addr := flag.String("addr", "", "microservice address")
-	flag.Parse()
-
-	path := ""
-	if *demo == "pub" {
-		path = "/publish"
-	} else if *demo == "sub" {
-		path = "/subscribe"
-	} else {
-		fmt.Println("-demo must have a valid value [pub,sub]")
-		return
-	}
-
-	if *addr == "" {
-		fmt.Println("-addr must have a valid value [addr:port")
-		return
-	}
-
-	url := url.URL{Scheme: "wss", Host: *addr, Path: path}
-	websocket.DefaultDialer.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	serviceConn, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	pubConn, err := dialToService("localhost:8081", "/publish", "hello", "test")
 
 	if err != nil {
-		fmt.Printf("%s [%s]", err, *addr)
+		fmt.Printf("[clientdemo] error dialing server\n%s", err)
 		return
 	}
 
-	if *demo == "pub" {
-		input := bufio.NewScanner(os.Stdin)
+	subConn, err := dialToService("localhost:8082", "/subscribe", "hello", "test")
 
-		for input.Scan() {
-			msg := input.Bytes()
-			fmt.Printf("Message SENT\n")
+	if err != nil {
+		fmt.Printf("[clientdemo] error dialing server\n%s", err)
+		return
+	}
 
-			err := serviceConn.WriteMessage(websocket.TextMessage, msg)
+	go readMessages(subConn)
+
+	writeMessages(pubConn, subConn)
+}
+
+func dialToService(addr string, path string, username string, password string) (*websocket.Conn, error) {
+	serviceURL := url.URL{Scheme: "wss", Host: addr, Path: path}
+	authHeader := http.Header{"Authorization": {"Basic " + base64.StdEncoding.EncodeToString([]byte(username+":"+password))}}
+	websocket.DefaultDialer.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	serviceConn, _, err := websocket.DefaultDialer.Dial(serviceURL.String(), authHeader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return serviceConn, nil
+}
+
+func readMessages(conn *websocket.Conn) {
+	for {
+		msg := &Message{}
+		err := conn.ReadJSON(msg)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("<[%s] %s\n", msg.Topic, msg.Content)
+	}
+}
+
+func writeMessages(pubConn *websocket.Conn, subConn *websocket.Conn) {
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		err := input.Err()
+
+		if err != nil {
+			fmt.Println("[clientdemo] error reading input")
+			return
+		}
+
+		msg := string(input.Bytes())
+
+		if msg == "exit" {
+			return
+		}
+
+		firstWhitespace := strings.Index(msg, " ")
+
+		if firstWhitespace == -1 {
+			fmt.Println("[clientdemo] Invalid syntax, correct one: [topic message] or [sub/unsub topic]")
+			continue
+		}
+
+		firstPart := msg[:firstWhitespace]
+		secondPart := msg[firstWhitespace+1:]
+
+		if firstPart == "sub" || firstPart == "unsub" {
+			err = subConn.WriteJSON(Message{secondPart, firstPart})
 
 			if err != nil {
 				fmt.Println(err)
-				return
+				continue
 			}
 
-			return
+			continue
 		}
 
-		if err := input.Err(); err != nil {
-			fmt.Println("error reading input")
-			return
+		err = pubConn.WriteJSON(Message{firstPart, secondPart})
+
+		if err != nil {
+			fmt.Println(err)
+			continue
 		}
-	} else {
-		go func() {
-			defer serviceConn.Close()
 
-			for {
-				_, msg, err := serviceConn.ReadMessage()
-
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				fmt.Printf("%s\n", msg)
-			}
-		}()
-
-		fmt.Println("Press ENTER to stop subscribing and end the program")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
+		fmt.Printf(">[%s] %s\n", firstPart, secondPart)
 	}
 }
--- subscriber/subscriber.go
+++ subscriber/subscriber.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"net/http"
@@ -12,54 +15,141 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool { //disable after done with js tests
+	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-var subscribed = make(map[*websocket.Conn]bool)
+var subscribers = make(map[string]map[*websocket.Conn]bool)
+
+type Message struct {
+	Topic   string
+	Content string
+}
 
 func main() {
-	url := url.URL{Scheme: "wss", Host: "localhost:8080", Path: "/popmsg"}
-	websocket.DefaultDialer.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	serviceConn, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	popConn, err := dialToService("localhost:8080", "/popmsg", "hello", "test")
 
-	if err != nil { // add retry later
-		fmt.Println(err)
+	if err != nil {
+		fmt.Printf("[subscriber] error dialing server\n%s", err)
 		return
 	}
 
-	go func() {
-		defer serviceConn.Close()
+	go popMessages(popConn)
 
-		for {
-			_, msg, err := serviceConn.ReadMessage()
+	ready := make(chan bool, 1)
+	err = initServer("localhost:8082", os.Getenv("WS_CERT_DIR"), ready)
 
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+	if err != nil {
+		fmt.Printf("[subscriber] error initialising server\n%s", err)
+	}
+}
 
-			for sub := range subscribed {
-				sub.WriteMessage(websocket.TextMessage, msg)
-			}
-		}
-	}()
+func dialToService(addr string, path string, username string, password string) (*websocket.Conn, error) {
+	serviceURL := url.URL{Scheme: "wss", Host: addr, Path: path}
+	authHeader := http.Header{"Authorization": {"Basic " + base64.StdEncoding.EncodeToString([]byte(username+":"+password))}}
+	websocket.DefaultDialer.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
-	http.HandleFunc("/subscribe", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
+	serviceConn, _, err := websocket.DefaultDialer.Dial(serviceURL.String(), authHeader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return serviceConn, nil
+}
+
+func popMessages(conn *websocket.Conn) {
+	for {
+		_, msg, err := conn.ReadMessage()
 
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		subscribed[conn] = true
+		fmt.Printf("[subscriber] received %s\n", msg)
+
+		var m Message
+		err = json.Unmarshal(msg, &m)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for sub := range subscribers[m.Topic] {
+			err := sub.WriteMessage(websocket.TextMessage, msg)
+
+			if err != nil {
+				fmt.Printf("[subscriber] error sending message to one subscriber\n%s", err)
+				continue
+			}
+
+			fmt.Printf("[subscriber] pushing %s\n", msg)
+		}
+	}
+}
+
+func initServer(addr string, certDir string, ch chan<- bool) error {
+	if certDir == "" {
+		err := errors.New("initServer: certDir is not defined")
+		return err
+	}
+
+	http.HandleFunc("/subscribe", func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+
+		if !ok || username != "hello" || password != "test" {
+			fmt.Printf("[subscriber] error validating credentials [%s:%s]\n", username, password)
+			return
+		}
+
+		conn, err := upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			fmt.Printf("[subscriber] error upgrading connection\n%s", err)
+			return
+		}
+
+		go func() {
+			for {
+				msg := &Message{}
+				err := conn.ReadJSON(msg)
+
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				if subscribers[msg.Topic] == nil {
+					subscribers[msg.Topic] = make(map[*websocket.Conn]bool)
+				}
+
+				if msg.Content == "sub" {
+					subscribers[msg.Topic][conn] = true
+					fmt.Printf("[subscriber] subscribed to %s\n", msg.Topic)
+					continue
+				}
+
+				if msg.Content == "unsub" {
+					delete(subscribers[msg.Topic], conn)
+					fmt.Printf("[subscriber] unsubscribed from %s\n", msg.Topic)
+					continue
+				}
+
+				fmt.Printf("[subscriber] ignoring invalid message %v\n", msg)
+			}
+		}()
 	})
 
-	err = http.ListenAndServeTLS("localhost:8082", os.Getenv("WS_CERT_DIR")+"server.crt", os.Getenv("WS_CERT_DIR")+"server.key", nil)
+	ch <- true
 
-	if err != nil {
-		fmt.Println(err)
+	err := http.ListenAndServeTLS(addr, certDir+"server.crt", certDir+"server.key", nil)
+
+	if err != http.ErrServerClosed {
+		return err
 	}
+
+	return nil
 }
